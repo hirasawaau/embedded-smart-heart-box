65dc07032bfa865670f5ed3840678d7305d94a0a
Sila Sonpee <hirasawa_au@yahoo.co.jp>
2024-03-03 01:37:25 +0700

    feat: Add more route

--- backend/main.go
+++ backend/main.go
@@ -62,6 +62,5 @@ func main() {
 		})
 	})
 
-	app.Listen(":3000")
+	app.Listen(":4000")
 }
-
--- backend/menu/router.go
+++ backend/menu/router.go
@@ -3,10 +3,13 @@
 package menu
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MenuRouter struct {
@@ -22,8 +25,29 @@ func NewMenuRouter(a *fiber.App, menuCol *mongo.Collection) {
 
 	s := "/:board"
 
-	grp.Post(s, router.CreateMenu)
+	grp.Put(s, router.EditMenus)
 	grp.Get(s, router.GetMenus)
+	grp.Post(s, router.ReplaceMenus)
+}
+
+func (r *MenuRouter) ReplaceMenus(c *fiber.Ctx) error {
+	board := c.Params("board")
+	_, err := r.menuCol.DeleteMany(c.Context(), bson.D{{"board_id", board}})
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	for i := 0; i < 3; i++ {
+		_, err = r.menuCol.InsertOne(c.Context(), MenuModel{
+			ID:      primitive.NewObjectID(),
+			Msg:     fmt.Sprintf("Menu %d", i+1),
+			BoardId: board,
+		})
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+	}
+	return c.JSON(fiber.Map{"msg": "ok"})
 }
 
 func (r *MenuRouter) CreateMenu(c *fiber.Ctx) error {
@@ -42,6 +66,45 @@ func (r *MenuRouter) CreateMenu(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(msg)
+}
+
+func (r *MenuRouter) EditMenus(c *fiber.Ctx) error {
+	body := new(EditMenuModel)
+	if err := c.BodyParser(body); err != nil {
+		return c.Status(400).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	if len(body.Menus) != 3 {
+		return c.Status(400).JSON(fiber.Map{"error": "Menus must be 3"})
+	}
+	findOpt := new(options.FindOptions)
+	findOpt.SetLimit(3)
+	cur, err := r.menuCol.Find(c.Context(), bson.D{
+		{"board_id", c.Params("board")},
+	}, findOpt)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+	i := 0
+	for cur.Next(c.Context()) {
+		var result MenuModel
+		err := cur.Decode(&result)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+		result.Msg = body.Menus[i]
+		i++
+
+		res, err := r.menuCol.UpdateOne(c.Context(), bson.D{
+			{"_id", result.ID},
+		}, bson.D{{"$set", result}})
+		fmt.Println(res)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+	}
+
+	return c.JSON(fiber.Map{"msg": "ok"})
 }
 
 func (r *MenuRouter) GetMenus(c *fiber.Ctx) error {
